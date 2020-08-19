afc5160fdfbed512b9e5bf33beeca5d6eea9c0a8
letidev <violeta@mlnova.com>
2020-08-19 13:52:54 +0300

    coffees module

---
+++ src/coffees/dto/update-coffee.dto.ts
@@ -0,0 +1,8 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCoffeeDto } from './create-coffee.dto';
+
+// All of the properties of the extended type are made optional.
+// Like this we remove the redundancy of repeating the same fields here and there
+// and the only difference being that here they are optional. PartialType
+// also inherits all of the validation rules set by the decorators.
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
